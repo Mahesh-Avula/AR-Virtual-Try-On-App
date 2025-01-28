4f28e004027dd678b9efec6056765acb27932a57
Mahesh-Avula <avulamahesh852@gmail.com>
2025-01-28 11:49:07 +0530

    main

---
+++ AR Virtual Try-On App/AR_Virtual_Try_On_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Virtual_Try_On_AppApp.swift
+//  AR Virtual Try-On App
+//
+//  Created by Mahesh Avula on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AR_Virtual_Try_On_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR Virtual Try-On App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AR Virtual Try-On App
+//
+//  Created by Mahesh Avula on 28/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
